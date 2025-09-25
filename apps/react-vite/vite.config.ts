f6674977b4b2aace8b872d9fedd02bda2e3c7734
CrossOmi <tsubasa-omi@x-power.co.jp>
2025-09-25 17:12:56 +0900

    その他テスト回り設定修正対応

--- apps/react-vite/vite.config.ts
+++ apps/react-vite/vite.config.ts
@@ -1,6 +1,10 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+// 1. Node.js built-in modules
+import { fileURLToPath, URL } from 'url';
+
+// 2. External packages
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
@@ -8,20 +12,17 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   base: './',
   plugins: [react(), viteTsconfigPaths()],
+  // ViteとVitestの両方でパスエイリアスを解決するための設定
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: {
     port: 3000,
   },
   preview: {
     port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/testing/setup-tests.ts',
-    exclude: ['**/node_modules/**', '**/e2e/**'],
-    coverage: {
-      include: ['src/**'],
-    },
   },
   optimizeDeps: { exclude: ['fsevents'] },
   build: {
